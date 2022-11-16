1b117e1807ee4dfce36358d63b2d07169a2e9369
DiasKarol <karol.gulbarg@gmail.com>
2022-11-16 13:47:50 -0300

    add queries GET e POST

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,19 @@
 import express from 'express';
 
+import { Database } from "./repositories/index";
 const app = express();
+
+// app.use('/get', () => {
+//     console.log("foi")
+//     const db = new Database();
+//     db.get_user('mail@mail.com');
+// }
+
+app.use('/', () => {
+    console.log("foi")
+    const db = new Database();
+    db.post_memberSquad('mail@mail.com');
+})
+
 
 app.listen(8000, ()=> console.log('tô on'));
